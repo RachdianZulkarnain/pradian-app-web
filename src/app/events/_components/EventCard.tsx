ab8edf01ba4f830456df8b54c8fbc0300193bf7e
RachdianZulkarnain <rachdianmuhammadadha@gmail.com>
2025-07-28 17:30:44 +0700

    add events

---
+++ src/app/events/_components/EventCard.tsx
@@ -0,0 +1,34 @@
+import { Card, CardContent, CardHeader } from '@/components/ui/card';
+import { Event } from '@/types/events';
+import Image from 'next/image';
+import Link from 'next/link';
+import React, { FC } from 'react'
+
+interface EventCardProps {
+  event: Event;
+}
+
+const EventCard: FC<EventCardProps> = ({ event }) => {
+  return (
+    <Link href={`/events/${event.slug}`}>
+      <Card>
+        <CardHeader>
+          <div className="relative h-[200px] w-full overflow-hidden rounded-lg">
+            <Image
+              src={event.thumbnail}
+              alt="thumbnail"
+              className="object-cover"
+              fill
+            />
+          </div>
+        </CardHeader>
+        <CardContent>
+          <h2 className="text-lg font-bold">{event.title}</h2>
+          <p className="line-clamp-3">{event.description}</p>
+        </CardContent>
+      </Card>
+    </Link>
+  );
+};
+
+export default EventCard
---
+++ src/app/events/hooks/useGetEvents.tsx
@@ -0,0 +1,25 @@
+import axiosInstance from "@/lib/axios";
+import { Event } from "@/types/events";
+import { PageableResponse, PaginationQueries } from "@/types/pagination";
+import { useQueries, useQuery } from "@tanstack/react-query";
+
+interface GetEventsQuery extends PaginationQueries {
+  search?: string;
+  category?: string;
+  location?: string;
+}
+
+const useGetEvents = (queries?: GetEventsQuery) => {
+  return useQuery({
+    queryKey: ["events", queries],
+    queryFn: async () => {
+      const { data } = await axiosInstance.get<PageableResponse<Event>>(
+        "/events",
+        { params: queries },
+      );
+      return data;
+    },
+  });
+};
+
+export default useGetEvents;
