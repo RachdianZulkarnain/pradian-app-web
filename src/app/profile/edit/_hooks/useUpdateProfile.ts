efb039dd48c881fe9996d0953f59bfe7b3fe07fd
DimPraditya <dimprad748296@gmail.com>
2025-08-01 13:48:46 +0700

    feat:profile

--- src/app/profile/edit/_hooks/useUpdateProfile.ts
+++ src/app/profile/edit/_hooks/useUpdateProfile.ts
@@ -1,7 +1,8 @@
+"use client"
+
 import axiosInstance from "@/lib/axios";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { AxiosError } from "axios";
-import { error } from "console";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 
