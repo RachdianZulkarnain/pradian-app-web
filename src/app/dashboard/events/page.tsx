e1723de3dc83e6720d86564ac33c7e7bb3cff48f
RachdianZulkarnain <rachdianmuhammadadha@gmail.com>
2025-08-01 13:48:44 +0700

    Feat:create event

--- src/app/dashboard/events/page.tsx
+++ src/app/dashboard/events/page.tsx
@@ -1,10 +1,11 @@
+import { notFound, redirect } from "next/navigation";
 import { auth } from "@/auth";
 import CreateEventPage from "./create/component/CreatePage";
 
 const DashboardEvents = async () => {
   const session = await auth();
 
-  // if (!session?.user) return notFound();
+  if (!session?.user) return notFound();
 
   return <CreateEventPage />;
 };
